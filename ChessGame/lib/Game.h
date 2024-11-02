ae818bbcd05ec6e55755b4b46ff346c73d2ed3cd
Phat Hoang <hdhphat2412@apcs.fitus.edu.vn>
2024-11-02 17:54:21 +0700

    Move game loop from main into two-player mode

--- ChessGame/lib/Game.h
+++ ChessGame/lib/Game.h
@@ -1,15 +1,18 @@
 ﻿#pragma once
-#include"../lib/StockFish.h"
+#include "../lib/StockFish.h"
+#include "../lib/ChessBoard.h"
 
 class Game {
 private:
 	bool againstAI; //True if play with AI
 	Stockfish stockfish;
+	sf::RenderWindow* window;
+
 public:
 	Game();
 	Game(bool againstAI, Stockfish& stockfish, int skillLevel = 0);
 
-	void startGame();
+	void startGame(sf::RenderWindow* win);
 	void playAgainstAI();
 	void playAgainstPlayer();
 };
