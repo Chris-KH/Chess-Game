46efff6d0bf083ef28dc7d196d3f799c3b4b2213
Chris-KH <klowgamervn@gmail.com>
2024-11-20 10:29:07 +0700

    Implement Move class

--- ChessGame/lib/Move..h
+++ ChessGame/lib/Move..h
@@ -2,20 +2,42 @@
 #include"Pieces.h"
 
 class Move {
-public:
-    // Vị trí cũ (x = row, y = col)
-    int fromX, fromY;    
-    // Vị trí mới (x = row, y = col)
-    int toX, toY;                  
-    Pieces* pieceMoved;            // Quân cờ được di chuyển
-    Pieces* pieceCaptured;         // Quân cờ bị ăn (nếu có)
-    bool isCastling;               // Có phải nhập thành không
-    bool isPromotion;              // Có phải phong cấp không
-    Pieces* promotionPiece;        // Loại quân cờ sau khi phong cấp
-    bool isEnPassant;              // Có phải bắt tốt qua đường không
-    bool castlingRights[2][2];     // Quyền nhập thành trước nước đi
+    // Old position (x = row, y = col)
+    int fromX, fromY;
+    // New position (x = row, y = col)
+    int toX, toY;
+    Pieces* pieceMoved;            // Moved piece
+    Pieces* pieceCaptured;         // Captured piece 
+    bool isCastling;               // Is a castling move
+    bool isPromotion;              // Is a protion move
+    Pieces* promotionPiece;        // Type of piece is promoted
+    bool isEnPassant;              // Is an en passant move
 
+    /*
+    castlingRights[0][0]: White's king-side castling right (short castling).
+    castlingRights[0][1]: White's queen-side castling right (long castling).
+    castlingRights[1][0]: Black's king-side castling right (short castling).
+    castlingRights[1][1]: Black's queen-side castling right (long castling).
+    */
+    bool castlingRights[2][2];     
+public:
     Move();
     Move(int fromX, int fromY, int toX, int toY);
-    Move(int fromX, int fromY, int toX, int toY, Pieces*& pieceMoved, Pieces*& pieceCaptured);
+    Move(int fromX, int fromY, int toX, int toY, Pieces*& pieceMoved, Pieces*& pieceCaptured, Pieces*& promotionPiece);
+    pair<int, int> getFrom() const;
+    pair<int, int> getTo() const;
+    void setCastling(bool isCastling);
+    void setPromotion(bool isPromotion);
+    void setEnPassant(bool isEnPassant);
+    bool getCastling() const;
+    bool getPromotion() const;
+    bool getEnPassant() const;
+
+    void setPieceMoved(Pieces*& pieceMoved);
+    void setPieceCaptured(Pieces*& pieceCaptured);
+    void setPromotionPiece(Pieces*& promotionPiece);
+
+    Pieces* getPieceMoved() const;
+    Pieces* getPieceCaptured() const;
+    Pieces* getPromotionPiece() const;
 };
--- ChessGame/src/Move.cpp
+++ ChessGame/src/Move.cpp
@@ -1,32 +1,93 @@
 ﻿#include"../lib/Move..h"
 
-Move::Move() {
-    fromX = fromY = 0;              
-    toX = toY = 0;                  
-    pieceMoved = nullptr;            
-    pieceCaptured = nullptr;         
+Move::Move() : fromX(0), fromY(0), toX(0), toY(0), pieceMoved(nullptr), pieceCaptured(nullptr), promotionPiece(nullptr) {         
     isCastling = false;;
     isPromotion = false;              
     promotionPiece = nullptr;        
     isEnPassant = false;              
-    //castlingRights[2][2];    
+    castlingRights[0][0] = false; 
+    castlingRights[0][1] = false; 
+    castlingRights[1][0] = false;
+    castlingRights[1][1] = false;
+
 }
 
-Move::Move(int fromX, int fromY, int toX, int toY): fromX(fromX), fromY(fromY), toX(toX), toY(toY) {
-    pieceMoved = nullptr;
-    pieceCaptured = nullptr;
+Move::Move(int fromX, int fromY, int toX, int toY)
+    : fromX(fromX), fromY(fromY), toX(toX), toY(toY), pieceMoved(nullptr), pieceCaptured(nullptr), promotionPiece(nullptr) {
     isCastling = false;;
     isPromotion = false;
     promotionPiece = nullptr;
     isEnPassant = false;
-    //castlingRights[2][2];
+    castlingRights[0][0] = false;
+    castlingRights[0][1] = false;
+    castlingRights[1][0] = false;
+    castlingRights[1][1] = false;
 }
 
-Move::Move(int fromX, int fromY, int toX, int toY, Pieces*& moved, Pieces*& captured)
-    : fromX(fromX), fromY(fromY), toX(toX), toY(toY), pieceMoved(moved), pieceCaptured(captured) {
+Move::Move(int fromX, int fromY, int toX, int toY, Pieces*& moved, Pieces*& captured, Pieces*& promotionPiece)
+    : fromX(fromX), fromY(fromY), toX(toX), toY(toY), pieceMoved(moved), pieceCaptured(captured), promotionPiece(promotionPiece) {
     isCastling = false;;
     isPromotion = false;
-    promotionPiece = nullptr;
     isEnPassant = false;
-    //castlingRights[2][2];
+    castlingRights[0][0] = false;
+    castlingRights[0][1] = false;
+    castlingRights[1][0] = false;
+    castlingRights[1][1] = false;
+}
+
+
+pair<int, int> Move::getFrom() const {
+    return make_pair(fromX, fromY);
+}
+
+pair<int, int> Move::getTo() const {
+    return make_pair(toX, toY);
+}
+
+void Move::setCastling(bool isCastling) {
+    this->isCastling = isCastling;
+}
+
+void Move::setPromotion(bool isPromotion) {
+    this->isPromotion = isPromotion;
+}
+
+void Move::setEnPassant(bool isEnPassant) {
+    this->isEnPassant = isEnPassant;
+}
+
+bool Move::getCastling() const {
+    return this->isCastling;
+}
+
+bool Move::getPromotion() const {
+    return this->isPromotion;
+}
+
+bool Move::getEnPassant() const {
+    return this->isEnPassant;
+}
+
+void Move::setPieceMoved(Pieces*& pieceMoved) {
+    this->pieceMoved = pieceMoved;
+}
+
+void Move::setPieceCaptured(Pieces*& pieceCaptured) {
+    this->pieceCaptured = pieceCaptured;
+}
+
+void Move::setPromotionPiece(Pieces*& promotionPiece) {
+    this->promotionPiece = promotionPiece;
+}
+
+Pieces* Move::getPieceMoved() const {
+    return this->pieceMoved;
+}
+
+Pieces* Move::getPieceCaptured() const {
+    return this->pieceCaptured;
+}
+
+Pieces* Move::getPromotionPiece() const {
+    return this->promotionPiece;
 }
